cf2e18b7fb55e07975313bc61b845f506d861f96
Made I.T <info@madeit.be>
2017-12-03 17:56:51 +0100

    Update SMSNetherlandTest.php

--- tests/SMSNetherlandTest.php
+++ tests/SMSNetherlandTest.php
@@ -15,7 +15,7 @@ class SMSNetherlandTest extends \PHPUnit_Framework_TestCase
     {
         $targetPay = new TargetPay(new \TPWeb\TargetPay\Transaction\SMS(IVR::NETHERLAND, 0.25), $this->config);
         $this->assertEquals(0.25, $targetPay->transaction->getAmount());
-        $this->assertEquals(3011, $targetPay->transaction->getShortcode());
+        $this->assertEquals(3010, $targetPay->transaction->getShortcode());
         $this->assertEquals("BETAAL AA", $targetPay->transaction->getKeyword());
     }
     
@@ -23,7 +23,7 @@ class SMSNetherlandTest extends \PHPUnit_Framework_TestCase
     {
         $targetPay = new TargetPay(new \TPWeb\TargetPay\Transaction\SMS(IVR::NETHERLAND, 1.50), $this->config);
         $this->assertEquals(1.50, $targetPay->transaction->getAmount());
-        $this->assertEquals(3011, $targetPay->transaction->getShortcode());
+        $this->assertEquals(3010, $targetPay->transaction->getShortcode());
         $this->assertEquals("BETAAL AL", $targetPay->transaction->getKeyword());
     }
     
@@ -31,7 +31,7 @@ class SMSNetherlandTest extends \PHPUnit_Framework_TestCase
     {
         $targetPay = new TargetPay(new \TPWeb\TargetPay\Transaction\SMS(IVR::NETHERLAND, 2.00), $this->config);
         $this->assertEquals(2.00, $targetPay->transaction->getAmount());
-        $this->assertEquals(3011, $targetPay->transaction->getShortcode());
+        $this->assertEquals(3010, $targetPay->transaction->getShortcode());
         $this->assertEquals("BETAAL AR", $targetPay->transaction->getKeyword());
     }
 }
